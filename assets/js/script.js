8cf5d6a60386b1de86b1b343d9927eeaad5d3796
Raghad Malallah <raghadmalallah@gmail.com>
2021-09-27 20:20:49 -0700

    adding div class in the body with h3 and p tag

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+// WHEN the page loads:
+
+// The following elements are shown:
+// - high scores link
+// - timer at 0
+// - title
+// - welcome message
+// - start button
+
+// WHEN the user clicks start
+
+// - Hide/remove title, welcome message, and start button
+
+// - Display question: What keyword can be used to print the type of a value?
+
+// - display buttons with the following text below the question:
+//   - A true
+//   - B string
+//   - C typeof
+//   - D function
+
+// - Start countdown: display "Time: 120" to start.
+// - Update the time display each second counting down.
+
+
+// WHEN the user clicks a button
+// IF the user clicked "C typeof" button
+
+
+// IF the user clicked "A true", "B string", or "D function"
+
+
+// WHEN the time remaining reaches 0 or less
